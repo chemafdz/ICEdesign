daa39ef82152a646526b5cc5ee584ce559327599
Sebastian Lopez <sebastianls0228@gmail.com>
2019-03-08 12:13:44 -0600

    Limpieza 2

--- Punto6.m
+++
@@ -1,5 +0,0 @@
-% Punto 6 - Verificar la potencia del ciclo termodinámico ideal.
-function [W_dot] = Punto6(W,Vmax,Vmin,N,d,l,K)
-    PMEI = W/(Vmax-Vmin);
-    W_dot = PMEI * l * .25*pi*d^2 * (N/2) * K;
-end
